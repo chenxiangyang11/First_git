7c02a75c3deab3e100a03a01f65cf3d67893d40d
chenxiangyang11 <1603897676@qq.com>
2026-05-16 20:09:06 +0800

    11

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     cout << "Hello, World!" << endl;
     cout << "This is a C++ program." << endl;
     return 0;
-
 }
